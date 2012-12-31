c6623a718f4cabda2b40059a2853279650e67805
Parth Parekh <parthparekh@gmail.com>
2012-12-31 04:33:35 -0800

    format file and add test

--- src/main/java/com/parthparekh/algorithms/AnagramSolver.java
+++ src/main/java/com/parthparekh/algorithms/AnagramSolver.java
@@ -9,7 +9,7 @@ import java.util.Set;
  * This class will implement the logic to solve multi-word anagrams
  *
  * @author: Parth Parekh (parthparekh [at] gatech [dot] edu)
- **/
+ */
 public class AnagramSolver {
 
     private int minWordSize = 3;
@@ -41,11 +41,11 @@ public class AnagramSolver {
 
         int count = 0;
         // check for all the words in key list for anagrams
-        for (int index=0; index<keyList.size(); index++) {
+        for (int index = 0; index < keyList.size(); index++) {
             char[] charInventory = wordString.toCharArray();
             Set<Set<String>> dictWordAnagramsSet = findAnagrams(index, charInventory, keyList);
             Set<Set<String>> tempAnagramSet = new HashSet<Set<String>>();
-            if (dictWordAnagramsSet!=null && !dictWordAnagramsSet.isEmpty()) {
+            if (dictWordAnagramsSet != null && !dictWordAnagramsSet.isEmpty()) {
                 Set<Set<String>> mergeResult = null;
                 for (Set<String> anagramSet : dictWordAnagramsSet) {
                     mergeResult = mergeAnagramKeyWords(anagramSet);
@@ -53,7 +53,7 @@ public class AnagramSolver {
                 }
                 System.out.println("");
                 System.out.println("\t(" + sortedDictionary.findSingleWordAnagrams(keyList.get(index)).toString()
-                                                                .replace(",", "") + ")");
+                        .replace(",", "") + ")");
                 for (Set<String> anagramSet : tempAnagramSet) {
                     System.out.println("" + ++count + ".\t" + anagramSet.toString().replace(",", ""));
                 }
@@ -69,7 +69,7 @@ public class AnagramSolver {
     // starting with the word at dictionaryIndex in dictionary keyList
     private Set<Set<String>> findAnagrams(int dictionaryIndex, char[] charInventory, List<String> keyList) {
         // terminating condition if no words are found
-        if (dictionaryIndex>=keyList.size() || charInventory.length<minWordSize) {
+        if (dictionaryIndex >= keyList.size() || charInventory.length < minWordSize) {
             return null;
         }
 
@@ -92,7 +92,7 @@ public class AnagramSolver {
             char[] newCharInventory = AnagramSolverHelper.setDifference(charInventory, searchWordChars);
             if (newCharInventory.length >= minWordSize) {
                 Set<Set<String>> anagramsSet = new HashSet<Set<String>>();
-                for (int index=dictionaryIndex+1; index<keyList.size(); index++) {
+                for (int index = dictionaryIndex + 1; index < keyList.size(); index++) {
                     Set<Set<String>> searchWordAnagramsKeysSet = findAnagrams(index, newCharInventory, keyList);
                     if (searchWordAnagramsKeysSet != null) {
                         Set<Set<String>> mergedSets = mergeWordToSets(searchWord, searchWordAnagramsKeysSet);
@@ -118,16 +118,17 @@ public class AnagramSolver {
             throw new IllegalStateException("anagram keys set cannot be null");
         }
         Set<Set<String>> anagramsSet = new HashSet<Set<String>>();
-        for(String word : anagramKeySet) {
+        for (String word : anagramKeySet) {
             Set<String> anagramWordSet = sortedDictionary.findSingleWordAnagrams(word);
             anagramsSet.add(anagramWordSet);
         }
         @SuppressWarnings("unchecked") // cannot use generics with Set array - Java bug???
-                Set<String>[] anagramsSetArray = anagramsSet.toArray(new Set[0]);
+        Set<String>[] anagramsSetArray = anagramsSet.toArray(new Set[0]);
 
         return AnagramSolverHelper.setMultiplication(anagramsSetArray);
     }
 
+    // add word to all the sets
     private Set<Set<String>> mergeWordToSets(String word, Set<Set<String>> sets) {
         assert !word.isEmpty();
         if (sets == null) {
@@ -135,7 +136,7 @@ public class AnagramSolver {
         }
         Set<Set<String>> mergedSets = new HashSet<Set<String>>();
         for (Set<String> set : sets) {
-            if (set==null) {
+            if (set == null) {
                 throw new IllegalStateException("anagram keys set cannot be null");
             }
             set.add(word);
@@ -152,7 +153,7 @@ public class AnagramSolver {
 
         System.out.println("Usage:");
         System.out.println("\tjava -cp AnagramSolver.jar com.parthparekh.algorithms.AnagramSolver " +
-                                "<absolute_path_to_wordlist_file> <min_word_length> <words_for_anagram_search>");
+                                    "<absolute_path_to_wordlist_file> <min_word_length> <words_for_anagram_search>");
         System.out.println("");
     }
 
@@ -164,13 +165,13 @@ public class AnagramSolver {
         String wordlistPath = args[0];
         assert !wordlistPath.isEmpty();
         Integer minWordLength = Integer.parseInt(args[1]);
-        if (minWordLength<=0) {
+        if (minWordLength <= 0) {
             // defaulting it to 3
             minWordLength = 3;
         }
         AnagramSolver anagramSolver = new AnagramSolver(minWordLength, wordlistPath);
         String anagramWords = "";
-        for (int index=2; index<args.length; index++) {
+        for (int index = 2; index < args.length; index++) {
             anagramWords += args[index];
             anagramWords += " ";
         }
@@ -179,10 +180,9 @@ public class AnagramSolver {
 
         Set<Set<String>> anagrams = anagramSolver.findAllAnagrams(anagramWords);
         System.out.println("");
-        if (anagrams==null || anagrams.isEmpty()) {
+        if (anagrams == null || anagrams.isEmpty()) {
             System.out.println("no anagrams found..");
-        }
-        else {
+        } else {
             System.out.println("Total " + anagrams.size() + " anagrams found");
         }
     }
